256da5d5fd20a3f12849dbc7414d4d4bb46534b5
Nam Eungyeong <nek1717@naver.com>
2024-03-10 20:13:12 +0900

    2024-03-10

---
+++ backjoon/stepBystep/step11/24262.js
@@ -0,0 +1,5 @@
+// 시간복잡도
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+console.log(1);
+console.log(0);
--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.js
+++
@@ -1,4 +0,0 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().trim();
-
-console.log(1);
-console.log(0);
