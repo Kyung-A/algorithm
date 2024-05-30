49cd6fcd601b7712132bb8ee1d525149ce97fccc
Nam Eungyeong <nek1717@naver.com>
2024-05-30 19:55:26 +0900

    2024-05-30

---
+++ udemy/sort/merge.js
@@ -0,0 +1,29 @@
+// 합병 정렬을 본격적으로 하기전 워밍업
+function merge(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+console.log(mergeSort([1, 10, 50], [2, 14, 99, 100]));
