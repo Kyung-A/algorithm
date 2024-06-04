69eadfa6a1a7982875116481127002e23157adfe
Nam Eungyeong <nek1717@naver.com>
2024-06-04 18:32:17 +0900

    2024-06-04

---
+++ udemy/sort/mergeSort.js
@@ -0,0 +1,40 @@
+function merge(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  // merge는 위에 left. right 값이 충족?이 되었을때 실행되는 형태?
+  return merge(left, right);
+}
+
+console.log(mergeSort([1, 6, 10, 3, 7, 2]));
